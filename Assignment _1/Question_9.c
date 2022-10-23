029aeebbecc8f5f96c6d50bc57fa4b7b815e3bb2
Ankith-Charan <116480127+Ankith-Charan@users.noreply.github.com>
2022-10-23 12:08:22 +0530

    Add files via upload

---
+++ Assignment _1/Question_9.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int HH,MM;
+    printf("Enter Time in format in format of HH;MM\n ");
+    scanf("%d:%d",&HH,&MM);
+    printf(" %d Hour and %d Minutes",HH,MM);
+    return 0;
+}
